63a438bdcda0ea98f8742f4699fb4c0a9b596eec
wualin <38272850+wualin@users.noreply.github.com>
2018-04-19 10:37:56 +0800

    Add files via upload

--- web/js/1.js
+++ web/js/1.js
@@ -1,4 +1,6 @@
 function ShowPic(a){
   var newaddress=a.getAttribute("href");
   document.getElementById("pic").src=newaddress;
+  var text=a.getAttribute("title");
+  document.getElementById("text").innerHTML=text;
 }
